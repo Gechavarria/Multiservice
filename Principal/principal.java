e711034a485b8b0070119339a7465d68dcbab77a
gerardoechavarriaalvarez <gerardoechavarriaalvarez@gmail.com>
2024-05-21 16:14:12 -0500

    the main is added

---
+++ Principal/principal.java
@@ -0,0 +1,12 @@
+package Principal;
+
+import Control.ControlJFLogin;
+
+public class principal {
+
+    public static void main(String[] args) {
+
+        ControlJFLogin ir = new ControlJFLogin();
+    }
+
+}
